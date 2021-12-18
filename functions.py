a0ba9b61ddef3ac1ef6886464561907b809a0a66
Avani1432009 <85622838+Avani1432009@users.noreply.github.com>
2021-12-18 17:04:50 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      lenovo
+#
+# Created:     18-12-2021
+# Copyright:   (c) lenovo 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+introString = "My name is Avani. My age is 12. I am studing in class 7th. I am studing in all Saints High School. I love arts and english."
+words = introString.split()
+print(words)
+phrases = introString.split(",")
+
+print(phrases)
+
+def greet(name):
+    print("hello,"+ name +".how are you?")
+
+greet('Avani')
